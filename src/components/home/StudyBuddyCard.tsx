387f9bbe0d27cc4a5fb5c2752721215da7778645
Filza Saleem <filza.saleem05@gmail.com>
2025-12-11 16:58:46 +0100

    changed the online status styling

--- src/components/home/StudyBuddyCard.tsx
+++ src/components/home/StudyBuddyCard.tsx
@@ -6,12 +6,10 @@ export default function StudyBuddyCard({ card }: { card: StudyBuddyT }) {
   return (
     <div className="studdybuddy-card">
       <div className="studdybuddy-card-initials">{card.initials}</div>
+      <div className="name-status">
       <h3 className="studdybuddy-card-name">{card.fullName}</h3>
-      <p
-        className={`studdybuddy-card-status ${card.isOnline ? "" : "offline"}`}
-      >
-        {card.isOnline ? "Online" : "Offline"}
-      </p>
+      <div className="status-dot" data-online={card.isOnline}></div>
+      </div>
 
       <div className="studdybuddy-card-subject">{card.subject}</div>
      <div className="studdybuddy-card-description">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deserunt, quidem!</div>
